b8a5da1caf1b3142479c010411e61403cac4ff66
Kerish <kerish.turista@gmail.com>
2023-04-03 08:30:01 +0500

    Added Linter

--- src/Cli.php
+++ src/Cli.php
@@ -2,7 +2,15 @@
 
 namespace BrainGames\Cli;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+// phpcs:disable
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+// phpcs:enable
 
 use function cli\line;
 use function cli\prompt;
