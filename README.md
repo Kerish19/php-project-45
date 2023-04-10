5e42870c02e40af6401ba4a16940821a7bd3b25f
Kerish <kerish.turista@gmail.com>
2023-04-10 07:09:19 +0500

    Step - 7. Brain GCD

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/dfe64be4bcc5d4ad322a/maintainability)](https://codeclimate.com/github/Kerish19/php-project-45/maintainability)
 #### Brain Even:
 [![asciicast](https://asciinema.org/a/8Ik0YmCDjzAQOPSr2dMArKu2c.svg)](https://asciinema.org/a/8Ik0YmCDjzAQOPSr2dMArKu2c)
+#### Brain GCD:
+[![asciicast](https://asciinema.org/a/tBmZ960GR1FV1bi9ICUeYgiZd.svg)](https://asciinema.org/a/tBmZ960GR1FV1bi9ICUeYgiZd)
---
+++ src/Games/GCD.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BrainGames\Games\GCD;
+
+use function BrainGames\Engine\runGame;
+
+const DESCRIPTION_GAME = 'Find the greatest common divisor of given numbers.';
+
+function calcGCD($firstOperand, $secondOperand)
+{
+    $gcd = 1;
+    $minOperand = $firstOperand < $secondOperand ? $firstOperand : $secondOperand;
+
+    for ($i = 1; $i <= $minOperand; $i++) {
+        if ($firstOperand % $i === 0 && $secondOperand % $i === 0) {
+            $gcd = $i;
+        }
+    }
+
+    return $gcd;
+}
+
+function startGCD()
+{
+    $genGameTask = function () {
+        $firstOperand = rand(1, 10);
+        $secondOperand = rand(1, 10);
+        $gameQuestion = "{$firstOperand} {$secondOperand}";
+        $answerQuestion = (string) calcGCD($firstOperand, $secondOperand);
+        return [$gameQuestion, $answerQuestion];
+    };
+
+    runGame(DESCRIPTION_GAME, $genGameTask);
+}
